83a651a12f88392657bde278899ff139eb0faf14
Olivier Butler <olivier.butler@gmail.com>
2017-10-08 13:56:11 -0700

    Built my first express server

---
+++ Express-v3/Enter-Express/server.js
@@ -0,0 +1,25 @@
+var express = require("express")
+var app = express()
+
+app.get('/', function(request, response){
+    response.send("<h1>Hi Guys!</h1>")
+})
+
+app.get('/stats', function(request, response){
+    response.send(`
+    <h1>Here be sexy stats</h1>
+    <h2>The request</h2>
+    <p>${request}</p>
+    <h2>The response</h2>
+    <p>${response}</p>
+    `)
+    console.log("The request!\n--------------\n")
+    console.log(request)
+    console.log("The response!\n--------------\n")
+    console.log(response)
+
+})
+
+app.listen(8882, function(){
+    console.log("We're listening on port 8882!")
+})
