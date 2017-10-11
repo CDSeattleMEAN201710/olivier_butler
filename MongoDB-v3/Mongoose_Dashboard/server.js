fd36cab0091cbaae8a08a9728bbbd3708fd01d6b
Olivier Butler <olivier.butler@gmail.com>
2017-10-10 18:23:37 -0700

    Display all view and route now working, db logic missing

--- MongoDB-v3/Mongoose_Dashboard/server.js
+++ MongoDB-v3/Mongoose_Dashboard/server.js
@@ -6,23 +6,27 @@ var mongoose = require("mongoose")
 var app = express()
 app.use(express.static(path.join(__dirname, './client/static')))
 app.use(bodyParser.urlencoded({extended: true}))
-app.set('views', path.join(__dirname, './client/static'))
-app.set('view controller', 'ejs')
+app.set('views', path.join(__dirname, './client/views'))
+app.set('view engine', 'ejs')
 
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/dashboard-1', {useMongoClient: true})
 
-app.get('/', function(){
-
+app.get('/', function(req, response){
+    const dumpJellyDB = [ 
+        { info: [1] }, 
+        { info: [1,1] }
+    ]
+    response.render('home', {jellys: dumpJellyDB})
 })
 app.get('/jelly/new', function(){
-    
+    response.render('new')
 })
 app.get('/jelly/:id', function(){
-    
+    response.render('single')
 })
 app.get('/jelly/edit/:id', function(){
-    
+    response.render('edit', {pageTitle : "Edit"})
 })
 
 app.post('/jelly', function(){
