a1f7a298f64179a3bb8184c89ac825682c8cba13
Olivier Butler <olivier.butler@gmail.com>
2017-10-14 22:27:06 -0700

    Finished timezone app

--- Angular/timeZones/src/app/app.component.ts
+++ Angular/timeZones/src/app/app.component.ts
@@ -14,15 +14,24 @@ export class AppComponent {
   time = new Date()
   offset
   zones = [
-    { name: 'Mexico', offset: -6 },
-    { name: 'Chad', offset: 1 },
-    { name: 'Belarus', offset: 3 },
-    { name: 'India', offset: 5.5 },
-    { name: 'China', offset: 8 }
+    { name: 'Mexico', offset: -6, active: false },
+    { name: 'Chad', offset: 1, active: false },
+    { name: 'Belarus', offset: 3, active: false },
+    { name: 'India', offset: 5.5, active: false },
+    { name: 'China', offset: 8, active: false }
   ]
-  adjustTime = (offset:number) => {
-    console.log(offset)
+  adjustTime = (inputIdx:number) => {
     console.log(this.time)
-    this.time = new Date(new Date().getTime() + (offset * 60 * 60000))
+    this.time = new Date(new Date().getTime() + (this.zones[inputIdx].offset * 3600000))
+    for (let totalIdx in this.zones){
+      if(Number(totalIdx) == inputIdx){
+        console.log(`Yay, ${this.zones[totalIdx].name}`)
+        this.zones[totalIdx].active = true
+      } else {
+        console.log(this.zones[totalIdx].name)
+        this.zones[totalIdx].active = false
+      }
+      //console.log(this.zones)
+    }
   }
 }
